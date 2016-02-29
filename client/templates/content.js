f00109da40efaf4154062e4a6c1fdf5c639d92de
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2016-02-29 16:14:45 +0800

    重构前

--- client/templates/content.js
+++ client/templates/content.js
@@ -2,14 +2,25 @@
  * Created by dengjing on 16/2/1.
  */
 
+var getCurrentLine = (currentNode, isEnter) =>{
+    let i = 0;
+    while(currentNode != null){
+        console.log(currentNode);
+        if(currentNode.data == '\n'){
+            i++;
+        }
+        currentNode = currentNode.previousSibling;
+    }
+    if(isEnter)
+        return i;
+    else
+        return i + 1;
+};
+
 Template.content.onCreated(
     () => {
         this.templateDictionary = new ReactiveDict();
-        this.templateDictionary.set('countID', 0);
-        //this.$('[name=textarea]').html("<b>Hello world!</b>");
-        //this.$('[name=textarea]').hide();
-        //this.templateDictionary.set('text', '');
-        //console.log('hahah' + this.$('[name=textarea]').val());
+        //this.templateDictionary.set('countID', 0);
     }
 );
 
@@ -21,21 +32,29 @@ Template.content.helpers({
 
 Template.content.events({
     'keyup .editor-content': (e) => {
-        var $textarea = $(e.target).find('[name=textarea]');
-        this.templateDictionary.set('text', $textarea.context.innerText);
+        if(e.keyCode == 13){
+            var $textarea = $(e.target).find('[name=textarea]');
+            this.templateDictionary.set('text', $textarea.context.innerText);
+            console.log(getCurrentLine(window.getSelection().getRangeAt(0).startContainer, true));
+        } else{
+            var $textarea = $(e.target).find('[name=textarea]');
+            this.templateDictionary.set('text', $textarea.context.innerText);
+            console.log(getCurrentLine(window.getSelection().getRangeAt(0).startContainer, false));
+        }
     },
-    //'keyup .editor-content': (e) => {
-    //    var $textarea = $(e.target).find('[name=textarea]');
-    //    Session.set('text', $textarea.context.innerText);
-        //console.log($textarea);
-        //new_content = "<span>" + $textarea.context.innerText + "</span>";
-        //$textarea.context.innerHTML= new_content;
-    //},
-    //'click .editor-content': (e) => {
-    //    console.log(e.currentTarget);
-    //    console.log(e);
-    //},
-    //'scroll .editor-control': (e) => {
-    //    console.log('haha');
-    //}
+
+
+    /*
+    'focus .editor-content': (e) => {
+        console.log(e);
+    },
+
+    'blur .editor-content': (e, t) => {
+        //console.log(e.window.getSelection().getRangeAt(0));
+        //window.getSelection().getRangeAt(0).setStart($(e.target).find('#star'), 0);
+        //console.log(window.getSelection().focusNode.parentNode);
+        console.log(window.getSelection().getRangeAt(0));
+        console.log(window.getSelection().getRangeAt(0).startContainer.previousSibling.previousSibling);
+    }
+    */
 });
