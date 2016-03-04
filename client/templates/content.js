99d8d8dbcde0d09a92baeec2111bc5e78a988314
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2016-03-04 18:29:18 +0800

    初具成型，将编辑那边换种方式计算行数，并标记

--- client/templates/content.js
+++ client/templates/content.js
@@ -4,31 +4,21 @@
 
 var baseFontSize = 58;
 
-var getCurrentLine = (currentNode, e) =>{ //已知的bug：1.回车后删除的节点浏览器认为还存在，影响计算line 2.第一行是回车，判断不是节点
-    let isEnter = false, i = 0;
-    if(e.keyCode == 13){
-        isEnter = true;
-    }else{
-        isEnter = false;
-    }
+var getCurrentLine = (currentRange) =>{ //已知的bug：如果是复制的内容会被当做一整块node，也有好处，解决方式，可以考虑格式化加上行数
+    var currentNode = currentRange.commonAncestorContainer;
+    var i = 1;
     while(currentNode != null){
-        if(currentNode.data == '\n'){
-            i++;
-        }
+        i = i + (currentNode.nodeValue || '').split('\n').length - 1;
         currentNode = currentNode.previousSibling;
     }
-    if(isEnter)
-        return i;
-    else
-        return i + 1;
+    return i;
 };
 
 var scrollPre = (e) => {
     var $textarea = $(e.target).find('[name=textarea]');
     this.templateDictionary.set('text', $textarea.context.innerText);
-    var line = getCurrentLine(window.getSelection().getRangeAt(0).startContainer, e);
+    var line = getCurrentLine(window.getSelection().getRangeAt(0));
     console.log(line);
-    console.log(this.$('#line-'+line).length);
     if(this.$('#line-'+line).length > 0){
         var h = this.$('#line-'+line)[0].offsetTop - baseFontSize;
         this.$('.preview-container').animate({scrollTop: h}, 50);
@@ -54,9 +44,6 @@ Template.content.helpers({
 
 Template.content.events({ //58 根据字体确定
     'keyup .editor-content': (e) => {
-        scrollPre(e);
-    },
-    'keydown .editor-content': (e) => {
         scrollPre(e);
     },
     'click .editor-content': (e) => {
