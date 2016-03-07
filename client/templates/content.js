5fdb835b20ca7aefd19f04a6a5ff79a284d3291b
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2016-03-07 12:42:16 +0800

    前端整个基本功能完成

--- client/templates/content.js
+++ client/templates/content.js
@@ -5,11 +5,8 @@
 var baseFontSize = 58;
 
 var getCurrentLine = (currentRange) => {
-    //已知的bug：如果是复制的内容会被当做一整块node，也有好处，解决方式，可以考虑格式化加上行数
-    //也可以使用监控86 keycode解析
     var currentNode = currentRange.commonAncestorContainer;
     var currentOffset = currentRange.startOffset;
-    console.log(currentOffset);
     var i = 1;
     if((currentNode != null) && (currentNode.nodeValue || '').split('\n').length == 2){ //第一次时候 计算当前位置的回车
         i = i + (currentNode.nodeValue || '').split('\n').length - 2;
@@ -21,7 +18,6 @@ var getCurrentLine = (currentRange) => {
         i = i + (nvalue || '').split('\n').length - 1;
         currentNode = currentNode.previousSibling;
     }
-    console.log(currentRange);
     return i;
 };
 
@@ -30,8 +26,6 @@ var scrollPre = (e) => {
     var $textarea = $(e.target).find('[name=textarea]');
     this.templateDictionary.set('text', $textarea.context.innerText);
     var line = getCurrentLine(window.getSelection().getRangeAt(0));
-    console.log(line);
-    console.log(this.$('#line-'+line));
     if(this.$('#line-'+line).length > 0){
         var h = this.$('#line-'+line)[0].offsetTop - baseFontSize;
         this.$('.preview-container').animate({scrollTop: h}, 50);
