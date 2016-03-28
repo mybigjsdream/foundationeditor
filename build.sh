5931be0bff3e5345011dbc843ff3c41e91a068b3
shanyue <554816284@qq.com>
2016-03-28 23:54:19 +0800

    部署文件改改

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 rm -rf ./build && meteor build ./build --directory && cd ./build/bundle/programs/server && npm install
 cd ../../../../
+rm -rf /var/www/blog
 mv ./build/bundle /var/www/blog
 cp ./ecosystem.json /var/www/blog/
+cd /var/www/blog
+pm2 delete blog
 pm2 start ecosystem.json
