e7946d53e207fd0c62ce75f013323d1f4e924e9a
shanyue <554816284@qq.com>
2016-03-30 13:28:43 +0800

    ..

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ rm -rf ./build && meteor build ./build --directory && cd ./build/bundle/programs
 cd ../../../../
 rm -rf /var/www/blog
 mv ./build/bundle /var/www/blog
+rm -rf ./build/
 cp ./ecosystem.json /var/www/blog/
 cd /var/www/blog
 pm2 delete blog
