5b353b2ba841c35699550d28c832b805831e07be
shanyu-e <554816284@qq.com>
2016-04-11 00:07:54 +0800

    bug fix 解决 作者 类别 对象 排序问题

--- client/templates/blog/blog_list/blog_sticky_list.js
+++ client/templates/blog/blog_list/blog_sticky_list.js
@@ -16,7 +16,7 @@ Template.blog_sticky_list.helpers({
     categories: () => {
         let ones = article_category_view.find({}).fetch();
         let ret_authors = {};
-        let ret_name = {};
+        let ret_name = [];
         ones.forEach((one) => {
             if(ret_authors[one.category]) {
                 ret_authors[one.category] += 1;
@@ -25,18 +25,16 @@ Template.blog_sticky_list.helpers({
             }
         });
         for(let key of Object.keys(ret_authors)){
-            ret_name[ret_authors[key]] = key
+            ret_name.push({name: key, value: ret_authors[key]});
         }
-        let last_return = [];
-        for(let key of Object.keys(ret_name).sort().reverse()){
-            last_return.push({'name': ret_name[key]});
-        }
-        return last_return.splice(0, 4);
+        ret_name.sort((a, b) => {return (a.value < b.value) ? 1 : ((b.value < a.value) ? -1 : 0);});
+        console.log(ret_name);
+        return ret_name.splice(0, 4);
     },
     authors: () => {
         let ones = article_author_view.find({}).fetch();
         let ret_authors = {};
-        let ret_name = {};
+        let ret_name = [];
         ones.forEach((one) => {
             if(ret_authors[one.userName]) {
                 ret_authors[one.userName] += 1;
@@ -45,12 +43,10 @@ Template.blog_sticky_list.helpers({
             }
         });
         for(let key of Object.keys(ret_authors)){
-            ret_name[ret_authors[key]] = key
+            ret_name.push({name: key, value: ret_authors[key]});
         }
-        let last_return = [];
-        for(let key of Object.keys(ret_name).sort().reverse()){
-            last_return.push({'name': ret_name[key]});
-        }
-        return last_return.splice(0, 4);
+        ret_name.sort((a, b) => {return (a.value < b.value) ? 1 : ((b.value < a.value) ? -1 : 0);});
+        console.log(ret_name);
+        return ret_name.splice(0, 4);
     },
 });
