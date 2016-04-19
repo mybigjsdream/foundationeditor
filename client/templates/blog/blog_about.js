2f92a1e90955cf4584e9853a4d7a9d2dec3c0c64
shanyu-e <554816284@qq.com>
2016-04-19 22:50:16 +0800

    随便写点吧，把文章update功能加上， 然后自动化脚本  git，  就开始写blog吧

--- client/templates/blog/blog_about.js
+++ client/templates/blog/blog_about.js
@@ -7,7 +7,7 @@ Template.blog_about.onRendered(
         let h = document.documentElement.clientHeight;
         let th = this.$('.blog_head').height();
         let fh = this.$('.footer').height();
-        this.$('.base-content').css('min-height', h-th-fh);
+        this.$('.base-content').css('min-height', h-th-fh-180);
     }
 );
 
