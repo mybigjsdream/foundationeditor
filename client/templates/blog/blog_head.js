e04078c72a48c5f8c9727cef2afaad9f1c33afdb
shanyu-e <554816284@qq.com>
2016-05-11 12:14:41 +0800

    数据查询请求

--- client/templates/blog/blog_head.js
+++ client/templates/blog/blog_head.js
@@ -12,4 +12,16 @@ Template.blog_head.events({
     'click #blog-about': (e) => {
         FlowRouter.go('/about');
     },
+    'click .button': (e) => {
+        let q = this.$('#search-text')[0].value;
+        const url = `http://182.92.220.227:8088/es/search?q=${q}`;
+        console.log(url);
+        Meteor.http.get(url, (e, r) => {
+            if(e)
+                console.log('http get FAILED!');
+            else{
+                console.log('success' + r);
+            }
+        });
+    }
 });
