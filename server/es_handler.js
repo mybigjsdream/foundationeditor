58d3190f1f859f376ebfe811f37b90b6819560af
shanyu-e <554816284@qq.com>
2016-05-12 17:55:17 +0800

    移到后端处理监控， meteor 这种机制 到底是好还是不好啊

--- server/es_handler.js
+++ server/es_handler.js
@@ -30,3 +30,20 @@ client.search({
     console.trace(error.message);
 });
 */
+import send_sqs from '../lib/utils/handle_sqs';
+
+var cursor = publish_article.find();
+cursor.observeChanges({
+    added: (id, fields) => {
+        console.log('star...add');
+        send_sqs('add', id, fields.en_title, fields.text);
+    },
+    changed: (id, fields) => {
+        console.log('star...change');
+        send_sqs('change', id, fields.en_title, fields.text);
+    },
+    removed: (id) => {
+        console.log('star...remove');
+        send_sqs('remove', id);
+    }
+});
